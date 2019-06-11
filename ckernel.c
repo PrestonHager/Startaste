8917efa36462e4be2e0dd55d3add7b888de4d5ba
Ubuntu <ubuntu@ip-172-31-28-173.us-west-2.compute.internal>
2019-06-11 22:30:57 +0000

    Added put_char function.

--- ckernel.c
+++ ckernel.c
@@ -8,5 +8,6 @@ void kernel_start() {
   char navigation_message[80] = "Nebula > Formation";
   graphics_clear(0x38, 0x90);
   graphics_update_navigation(navigation_message);
+  graphics_put_char('>', 0, 1);
   graphics_move_cursor(1, 1);
 }
--- libs/graphics.h
+++ libs/graphics.h
@@ -5,4 +5,5 @@
 extern void graphics_clear(unsigned char top_color, unsigned char main_color);
 extern void graphics_update_navigation(unsigned char msg[]);
 extern void graphics_print(unsigned char msg[], unsigned char x, unsigned char y);
+extern void graphics_put_char(unsigned char c, unsigned char x, unsigned char y);
 extern void graphics_move_cursor(unsigned char x, unsigned char y);
